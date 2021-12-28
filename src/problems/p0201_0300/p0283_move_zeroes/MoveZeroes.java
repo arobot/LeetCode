b9c04e46efbd7648f1b1780e52dff9511409c36e
luowei <luowei@jiuaoedu.com>
2021-12-28 17:50:27 +0800

    调整算法效率

--- src/problems/p0201_0300/p0283_move_zeroes/MoveZeroes.java
+++ src/problems/p0201_0300/p0283_move_zeroes/MoveZeroes.java
@@ -1,25 +1,19 @@
 package problems.p0201_0300.p0283_move_zeroes;
 
-import utils.ArrayUtils;
 import utils.PrintUtils;
 
 public class MoveZeroes {
     public static void moveZeroes(int[] nums) {
-        int continuousZero = 0;
+        int j = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0)
-                continuousZero++;
-            else
-                ArrayUtils.swap(nums, i, i - continuousZero);
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
+            }
         }
-    }
-
-    static void swap(int[] nums, int aIndex, int bIndex) {
-        if (nums[aIndex] == nums[bIndex])
-            return;
-        nums[aIndex] ^= nums[bIndex];
-        nums[bIndex] ^= nums[aIndex];
-        nums[aIndex] ^= nums[bIndex];
+        for (int i = j; i < nums.length; i++) {
+            nums[i] = 0;
+        }
     }
 
     public static void main(String[] args) {
