a9ddb8e47b1d2fbf41086f9145155d4c2723d9ab
luowei <niweigede@163.com>
2022-01-11 15:18:09 +0800

    0098 中序遍历

--- src/problems/p0001_0100/p0098_validate_binary_search_tree/README.md
+++ src/problems/p0001_0100/p0098_validate_binary_search_tree/README.md
@@ -10,4 +10,46 @@
 
 ## 分析
 
-递归思想第一时间上头，一次判定左右子树是否满足定义
+递归思想第一时间上头，一次次判定左右子树是否满足定义。
+
+### 1. 带上下限递归
+
+```java
+    public static boolean isValidBST(TreeNode root) {
+        return valid(root, Long.valueOf(Integer.MIN_VALUE) - 1, Long.valueOf(Integer.MAX_VALUE) + 1);
+    }
+
+    public static boolean valid(TreeNode node, Long min, Long max) {
+        if (node == null)
+            return true;
+        if (node.val <= min || max <= node.val)
+            return false;
+        return valid(node.left, min, Long.valueOf(node.val)) && valid(node.right, Long.valueOf(node.val), max);
+    }
+```
+
+这里之所以要带上上下限一起参与递归，是因为对于任意节点的子树，其左子树上的所有节点值必须小于该节点的值，同时其右子树上所有节点值必须大于该节点的值。
+
+### 2. 中序遍历
+
+这个思路，只能说远离数据结构太久了。第一时间想到了，也没有往解题的方向来思考。中序遍历的结果和题目要求的顺序一致，可以依次判断遍历的结果是否满足要求即可。
+
+```java
+    //中序遍历
+    static TreeNode prev;
+
+    public static boolean validMiddleFirst(TreeNode root) {
+        if (root == null)
+            return true;
+        if (!validMiddleFirst(root.left))
+            return false;
+        if (prev != null && prev.val >= root.val)
+            return false;
+        prev = root;
+        if (!validMiddleFirst(root.right))
+            return false;
+        return true;
+    }
+```
+
+中序遍历到最左叶子节点时开始赋值并逐渐递归出栈开始进行判定。
--- src/problems/p0001_0100/p0098_validate_binary_search_tree/ValidateBinarySerachTree.java
+++ src/problems/p0001_0100/p0098_validate_binary_search_tree/ValidateBinarySerachTree.java
@@ -1,11 +1,10 @@
 package problems.p0001_0100.p0098_validate_binary_search_tree;
 
 import utils.node.TreeNode;
-import utils.node.TreeUtils;
 
 public class ValidateBinarySerachTree {
     public static boolean isValidBST(TreeNode root) {
-        return valid(root, Long.valueOf(Integer.MIN_VALUE)-1, Long.valueOf(Integer.MAX_VALUE)+1);
+        return valid(root, Long.valueOf(Integer.MIN_VALUE) - 1, Long.valueOf(Integer.MAX_VALUE) + 1);
     }
 
     public static boolean valid(TreeNode node, Long min, Long max) {
@@ -16,9 +15,23 @@ public class ValidateBinarySerachTree {
         return valid(node.left, min, Long.valueOf(node.val)) && valid(node.right, Long.valueOf(node.val), max);
     }
 
+    //中序遍历
+    static TreeNode prev;
+
+    public static boolean validMiddleFirst(TreeNode root) {
+        if (root == null)
+            return true;
+        if (!validMiddleFirst(root.left))
+            return false;
+        if (prev != null && prev.val >= root.val)
+            return false;
+        prev = root;
+        if (!validMiddleFirst(root.right))
+            return false;
+        return true;
+    }
+
     public static void main(String[] args) {
-        // TreeNode root = TreeUtils.fullBinaryTree(1, 0, 2);
-        // root.left = new TreeNode(-2);
         TreeNode root = new TreeNode(2,
                 new TreeNode(1),
                 new TreeNode(6, new TreeNode(3), new TreeNode(7)));
@@ -26,10 +39,11 @@ public class ValidateBinarySerachTree {
         // root = new TreeNode(1, null, new TreeNode(1));
 
         // root = new TreeNode(32,
-        //         new TreeNode(26, new TreeNode(19, null, new TreeNode(27)), null),
-        //         new TreeNode(47, null, new TreeNode(56)));
+        // new TreeNode(26, new TreeNode(19, null, new TreeNode(27)), null),
+        // new TreeNode(47, null, new TreeNode(56)));
 
         // root = new TreeNode(Integer.MAX_VALUE);
         System.out.println(isValidBST(root));
+        validMiddleFirst(root);
     }
 }
