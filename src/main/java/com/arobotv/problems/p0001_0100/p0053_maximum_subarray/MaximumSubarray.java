19cf0d449abd9e125f2521b72ccd72b5a65ae97b
ennis <niweigede@163.com>
2022-01-19 00:22:51 +0800

    0053

---
+++ src/main/java/com/arobotv/problems/p0001_0100/p0053_maximum_subarray/MaximumSubarray.java
@@ -0,0 +1,24 @@
+package com.arobotv.problems.p0001_0100.p0053_maximum_subarray;
+
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int[] dp = new int[nums.length];
+        dp[0] = max;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            max = Math.max(dp[i], max);
+        }
+        return max;
+    }
+
+    public int maxSubArray2(int[] nums) {
+        int max = nums[0];
+        int cur = max;
+        for (int i = 1; i < nums.length; i++) {
+            cur = Math.max(cur, 0) + nums[i];
+            max = Math.max(cur, max);
+        }
+        return max;
+    }
+}
