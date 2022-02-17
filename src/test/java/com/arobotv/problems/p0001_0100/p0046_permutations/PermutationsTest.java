77254e3ddae012d591b8763f8cf4c5ca1f41b420
luowei <niweigede@163.com>
2022-02-17 11:00:21 +0800

    0046

---
+++ src/test/java/com/arobotv/problems/p0001_0100/p0046_permutations/PermutationsTest.java
@@ -0,0 +1,17 @@
+package com.arobotv.problems.p0001_0100.p0046_permutations;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PermutationsTest {
+    @Test
+    void testPermute() {
+        Permutations permutations = new Permutations();
+        Assertions.assertEquals("[[1]]", permutations.permute(new int[] { 1 }).toString());
+        Assertions.assertEquals("[[1, 2], [2, 1]]", permutations.permute(new int[] { 1, 2 }).toString());
+        Assertions.assertEquals("[[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]", permutations.permute(new int[] { 1, 2 ,3}).toString());
+        Assertions.assertEquals("[[1]]", permutations.permute2(new int[] { 1 }).toString());
+        Assertions.assertEquals("[[1, 2], [2, 1]]", permutations.permute2(new int[] { 1, 2 }).toString());
+        Assertions.assertEquals("[[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]", permutations.permute2(new int[] { 1, 2 ,3}).toString());
+    }
+}
