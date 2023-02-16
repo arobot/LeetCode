6e229ed5f9030af52b3d239ba20b9baeeeebc536
ennis <niweigede@163.com>
2023-02-16 10:37:44 +0800

    63

--- src/main/java/com/arobotv/Application.java
+++ src/main/java/com/arobotv/Application.java
@@ -2,6 +2,20 @@ package com.arobotv;
 
 public class Application {
     public static void main(String[] args) {
-        
+        System.out.println("m,n:3,3:" + uniquePath(3, 3));
+    }
+
+    public static int uniquePath(int m, int n) {
+
+        int[] row = new int[m];
+        for (int index = 0; index < row.length; index++) {
+            row[index] = 1;
+        }
+        for (int col = 1; col < n; col++) {
+            for (int index = 1; index < row.length; index++) {
+                row[index] = row[index - 1] + row[index];
+            }
+        }
+        return row[m - 1];
     }
 }
---
+++ src/main/java/com/arobotv/problems/p0001_0100/p0063_unique_paths2/UniquePaths2.java
@@ -0,0 +1,33 @@
+package com.arobotv.problems.p0001_0100.p0063_unique_paths2;
+
+public class UniquePaths2 {
+    public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    obstacleGrid[i][j] = 0;
+                    continue;
+                }
+                if (i == 0 && j == 0) {
+                    obstacleGrid[0][0] = 1;
+                    continue;
+                }
+                if (i == 0) {
+                    obstacleGrid[0][j] = obstacleGrid[0][j - 1];
+                } else if (j == 0)
+                    obstacleGrid[i][0] = obstacleGrid[i - 1][0];
+                else
+                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+            }
+        }
+        return obstacleGrid[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        int[][] a = {{0,1},{0,0}};
+        System.out.println("result:" + uniquePathsWithObstacles(a));
+    }
+
+}
